25404115e1b33c7305a1038af731b1f4851c6fb8
danielgoncales18 <danielgoncalesferreira16@gmail.com>
2026-07-29 16:39:58 -0300

    aula 02 - classe e objeto

--- aula-02/POO/Program.cs
+++ aula-02/POO/Program.cs
@@ -1,1 +1,65 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // comentario de linha
+        /*
+         comentario de bloco ou
+         comentario de varias linhas
+        */
+       
+         // escreve  uma linha de texto na tela 
+        Console.WriteLine("programação orientada a objetos\n");
+
+        Console.WriteLine("abstração - 1º pilar da poo");
+
+        /* criar um classe em c# 
+           a classe deve ser declarada fora do bloco de programa
+        */
+        
+         /* criar um objeto a partir de uma classe */
+           
+           pet pet1 = new pet();
+           pet pet1 = new pet();
+           pet1.nome = "toto";
+           pet1.tipo = "cachorro";
+           pet1.idade = 3;   
+           pet1.mostrarnome();
+
+
+
+
+
+           pet pet2 = new pet();
+           pet2.nome = "mingau";
+           pet2.tipo = "gato";
+           pet2.idade = 8;
+           pet2.mostrarnome();
+
+
+
+        }
+
+          
+
+
+
+
+    public class pet  // nome da classe sempre inicial com letras maiusculas 
+    {
+         // declaração dos atributos 
+           public string nome;
+           public string tipo;
+            public string idade;
+           
+            // declaração dos metodos
+            public void mostrarnome()
+         {
+            Console.WriteLine("meu nome é " + this.nome);
+         }
+    
+
+    }
+}
--- aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c31bb931b8ff94b49f541988240a7b30318e07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e3bf3e51bcaf5786ef096c13dc2962e2760f84")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

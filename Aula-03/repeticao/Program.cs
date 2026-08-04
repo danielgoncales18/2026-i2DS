717a18c32034ce93b725dbb5c69c350a709fba31
danielgoncales18 <danielgoncalesferreira16@gmail.com>
2026-08-04 14:21:15 -0300

    laços de repetição

--- Aula-03/repeticao/Program.cs
+++ Aula-03/repeticao/Program.cs
@@ -2,6 +2,67 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       /* 
+          sintaxe:
+            enquanto uma condição for verdadeira
+            execute um conjunto de comandos 
+
+        for -> quando sabemos a quantidade de repetições.
+        while -> quando a repetição depende de uma condição.
+        do...while -> quando precisa execultar ao menos uma vez o bloco de codigo
+       */
+
+       /* laco for: imprimir numeros de 1 até 10*/
+
+       Console.WriteLine("imprimir numeros de 1 até 10, ultilizandoo laco for.");
+        for (int numero = 1; numero <= 10; numero++)
+        {
+            Console.WriteLine(numero);
+        }  
+    
+    
+    /* laco for: contagem regressiva de 10 até 0*/
+
+       Console.WriteLine("imprimir contagem regressiva.");
+        for (int numero = 10; numero >= 0; numero--)
+        {
+            Console.WriteLine(numero);
+        }  
+    
+          /*********************************************************************/    
+      
+          
+          Console.WriteLine("imprimir numeros de 1 até 10, ultilizandoo laco while.");
+
+          int contador = 1;
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador++;
+
+        }
+
+        /* solicitar a senha ate acertar*/
+
+        string senha = "";
+
+     while(senha != "2444")
+        {
+            Console.Write("digite a senha:");
+
+            senha = Console.ReadLine();
+        }
+
+        Console.WriteLine("acesso permitido!");
+
+         /***************************************************************************************/
+         int num;
+        do
+        {
+            Console.WriteLine("digite um numero positivo:");
+            num = int.Parse(Console.ReadLine());
+        }while(num <=0);
+
+
     }
 }
--- Aula-03/repeticao/obj/Debug/net10.0/repeticao.AssemblyInfo.cs
+++ Aula-03/repeticao/obj/Debug/net10.0/repeticao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("repeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25404115e1b33c7305a1038af731b1f4851c6fb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f27622752439025ffc0924596afd8f91e6d32f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("repeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("repeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

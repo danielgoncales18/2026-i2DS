f0fea203c66c6135f5676f3d21452a481185f440
danielgoncales18 <danielgoncalesferreira16@gmail.com>
2026-08-05 15:51:37 -0300

    aula 04

--- Aula-03/exercicios/exercicio-1/obj/Debug/net10.0/exercicio-1.AssemblyInfo.cs
+++ Aula-03/exercicios/exercicio-1/obj/Debug/net10.0/exercicio-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercicio-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f319874004998b927d3efd30370e1db6336e205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb70e3dd9793183c0957b288b92a92652ca9569")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercicio-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercicio-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Aula-04/01-abstração/classeObjeto/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Dynamic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+          classe -> é a abstração de um objeto do mundo real para o mundo computacional.
+          objeto -> é a instancia de uma classe
+        */
+
+
+          // instanciar um objeto do tipo aluno
+          Aluno aluno01 = new Aluno();
+          Aluno aluno02 = new Aluno();
+
+          aluno01.Nome = "josé da silve";
+          aluno01.RM = 2444;
+          aluno01.Datanascimento = new DateOnly(2010,01,15);
+
+
+          aluno02.Nome = "joão dos sasntos";
+          aluno02.RM = 2444;
+          aluno02.Datanascimento = new DateOnly(2006,12,07);
+
+          // execultar os metodos
+          aluno01.Apresentarse();
+          aluno02.Apresentarse();
+
+
+
+ }
+
+        public class Aluno // declaração de uma classe
+    {
+        // atributos -> caracteristicas
+        public string Nome { get; set; } = string.Empty;
+        public int RM { get; set; }
+
+        public DateOnly Datanascimento { get; set; }
+
+
+        // metodos -> ações ou funcionalidades
+        public void Apresentarse()
+        {
+            Console.WriteLine($"olá, meu nome é {Nome}, Meu RM é {RM}, nasci na Data {Datanascimento}");
+        }
+
+    
+    }
+
+
+
+}
+
+
